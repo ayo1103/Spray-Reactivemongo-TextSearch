69fd1b031b9a84f03c18e8948ab3d191ef675bf5
Bryan Lin <ayo1103@gmail.com>
2014-11-10 14:16:27 +0800

    Adding log4j2.xml

--- src/main/scala/MyServiceActor.scala
+++ src/main/scala/MyServiceActor.scala
@@ -1,14 +1,29 @@
 import spray.http._
 import spray.routing._
 import StatusCodes._
+import reactivemongo.api.MongoDriver
+import scala.concurrent.ExecutionContext.Implicits.global
 
 class MyServiceActor extends HttpServiceActor {
+
+  // a mongoDriver instance manages an actor system
+  // using existing actorySystem -- context.system
+  val driver = new MongoDriver(context.system)
+
+  // a connect manages a pool of connections
+  val connection = driver.connection(List("localhost"))
+
+  // gets a reference to the database "spray-reactivemongo-textsearch"
+  val db = connection.db("spray-reactivemongo-textsearch")
+  val collection = db.collection("testDatas")
+
   def receive = runRoute {
-    get {
-      complete {
-        "Hello world"
+    path("testDatas") {
+      get {
+        complete {
+          "Hello world"
+        }
       }
     }
   }
-
 }
